d75e16f2b1d47dbace82ad4a03ae8d708804b594
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-07-28 23:51:39 +0300

    fix: toggle  cards in progress

---
+++ src/js/app.js
@@ -0,0 +1,33 @@
+// $(document).ready(function()
+// {
+
+//     $('.toggle-button').on('click', function(){
+//         $(this).next().slideToggle(200)
+//         $(this).find('span').toggleClass('rotate')
+//     })
+
+
+// });
+
+$(document).ready(function()
+{
+
+    $('.toggle-button').on('click', function(){
+        $(this).next().slideToggle(200)
+        $('button').find('span').toggleClass('rotate')
+    })
+
+
+});
+
+
+// $(document).ready(function()
+// {
+
+//     $('.toggle-button').on('click', function(){
+//         $(this).next().slideToggle(200)
+//         $(this).find('span').toggleClass('rotate')
+//     })
+
+
+// });
