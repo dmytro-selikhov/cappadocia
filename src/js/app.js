c1b80a0de6f659c3ad2254f7cfbb6337ec220424
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-07-29 12:55:08 +0300

    fix: top.scss section

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,10 @@
-// $(document).ready(function()
-// {
-
-//     $('.toggle-button').on('click', function(){
-//         $(this).next().slideToggle(200)
-//         $(this).find('span').toggleClass('rotate')
-//     })
-
-
-// });
-
 $(document).ready(function()
 {
 
     $('.toggle-button').on('click', function(){
         $(this).next().slideToggle(200)
-        $('button').find('span').toggleClass('rotate')
+        $(this).find('span').toggleClass('rotate')
     })
 
 
 });
-
-
-// $(document).ready(function()
-// {
-
-//     $('.toggle-button').on('click', function(){
-//         $(this).next().slideToggle(200)
-//         $(this).find('span').toggleClass('rotate')
-//     })
-
-
-// });
